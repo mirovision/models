9e9b37846fded7758dc75f8cd84deb8207c3f25b
marc-1212 <marc1.puig-arocas@epitech.eu>
2022-12-13 05:39:00 -0800

    [ADD] create stream class

--- models/Hot_dog_detection.py
+++ models/Hot_dog_detection.py
@@ -1,30 +1,28 @@
 from transformers import AutoFeatureExtractor, AutoModelForImageClassification
 from PIL import Image, ImageDraw, ImageFont
-import requests
-from CVModel import CVModel
+import requests 
+import io
+from models.CVModel import CVModel
 
 
 class HotDog(CVModel):
-    def __init__(self):
+    def   __init__(self):
         self.extractor = AutoFeatureExtractor.from_pretrained("julien-c/hotdog-not-hotdog")
-
         self.model = AutoModelForImageClassification.from_pretrained("julien-c/hotdog-not-hotdog")
         super().__init__()
 
-    def try_out(self, image):
-
+    def calculate(self, image):
         encoded_output = self.extractor(images=image,return_tensors="pt")
         output = self.model(**encoded_output)
         logits = output.logits
         predicted_class_idx = logits.argmax(-1).item()
-        self. hot_dog = self.model.config.id2label[predicted_class_idx]
+        self.hot_dog = self.model.config.id2label[predicted_class_idx]
 
-    def edit_frame(self, image, position, font,  font_size):
-            
-        edit_img = ImageDraw.Draw(image) 
-        print("hola")       
-        font = ImageFont.truetype(font , font_size)
-        edit_img.text((position[0], position[1]),  self.hot_dog, font=font, fill=(255,255,255))
-        image.save("edited_foto.jpg")
+    def draw(self, image):
+        edit_img = ImageDraw.Draw(image)
+        font = ImageFont.truetype('local_test/hot_dog_font.ttf' , 20)
+        edit_img.text((400,400), text=self.hot_dog ,font=font, fill=(255,255,255))
+        return image
+
        # to see the result you need to save it as this example
         #image.save("edited_butifarra.jpg")
