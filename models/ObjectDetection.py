ef9af305f98be3f12711a059e48ff484742adf93
marc-1212 <marc1.puig-arocas@epitech.eu>
2022-12-16 04:47:52 -0800

    [ADD] webcam with diff urls

--- models/ObjectDetection.py
+++ models/ObjectDetection.py
@@ -19,14 +19,7 @@ import torch
 class ObjectDetection(CVModel):
 
     def __init__(self):
-        super().__init__() 
-        self.____feature_extractor = None
-        self.__model = None
-        self.__image = None
-        self.__inputs = None
-        self.__outputs = None
-        self.__results = None
-        self.__result_image = None
+        super().__init__()
         self.__feature_extractor = DetrFeatureExtractor.from_pretrained("facebook/detr-resnet-50")
         self.__model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50")
         return
@@ -43,24 +36,9 @@ class ObjectDetection(CVModel):
         self.__result_image = np.array(self.__image)
         for score, label, box in zip(self.__results["scores"], self.__results["labels"], self.__results["boxes"]):
             box = [round(i, 2) for i in box.tolist()]
-            # let's only keep detections with score > 0.
-            # 9
             if score > 0.9:
-                #self.parsed_outputs.append({"box": [int(box[0]), int(box[1]), int(box[2]), int(box[3])], "item": self.__model.config.id2label[label.item()], "confidence": round(score.item(), 3)})
                 cv2.rectangle(self.__result_image, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 255, 0), 2)
                 text = self.__model.config.id2label[label.item()] + ": " + str(round(score.item(), 3))
-                cv2.putText(self.__result_image, text, (int(box[0]+5),int(box[1]+13)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        
+                cv2.putText(self.__result_image, text, (int(box[0]+5),int(box[1]+13)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)    
         result  = Image.fromarray(self.__result_image)
-        return result
-
-    
-    def output(self):
-        super().output()
-        target_sizes = torch.tensor([self.__image.size[::-1]])
-        self.__results = self.__feature_extractor.post_process_object_detection(self.__outputs, target_sizes=target_sizes)[0]
-        # image = self.__draw_output()
-        # image.save("sample.jpg")
-        result = self.__draw_output()
-        #success, encoded_image = cv2.imencode('.jpg', result)
         return result
